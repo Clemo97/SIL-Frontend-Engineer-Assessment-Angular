d37e865a3a46852320877422a2b345ffcf071089
Clemo97 <lumumbaclement@gmail.com>
2024-02-06 16:46:09 +0300

    firebase setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: "AIzaSyCD9VuJIlLIZAEvEIdwdIFeinlUMoPMVTE",
+  authDomain: "sil-hosting.firebaseapp.com",
+  projectId: "sil-hosting",
+  storageBucket: "sil-hosting.appspot.com",
+  messagingSenderId: "675416219162",
+  appId: "1:675416219162:web:78958919f2fa479b19fba3"
 };
