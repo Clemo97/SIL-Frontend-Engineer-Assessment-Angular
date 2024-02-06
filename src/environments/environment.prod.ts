2dac3577e2caada424167bba792ddf7f98987293
Clemo97 <lumumbaclement@gmail.com>
2024-02-06 17:03:50 +0300

    set correct environment keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,12 @@
 export const environment = {
   production: true,
-  apiKey: "AIzaSyCD9VuJIlLIZAEvEIdwdIFeinlUMoPMVTE",
-  authDomain: "sil-hosting.firebaseapp.com",
-  projectId: "sil-hosting",
-  storageBucket: "sil-hosting.appspot.com",
-  messagingSenderId: "675416219162",
-  appId: "1:675416219162:web:78958919f2fa479b19fba3"
+  firebase: {
+    apiKey: "AIzaSyCD9VuJIlLIZAEvEIdwdIFeinlUMoPMVTE",
+    authDomain: "sil-hosting.firebaseapp.com",
+    projectId: "sil-hosting",
+    storageBucket: "sil-hosting.appspot.com",
+    messagingSenderId: "675416219162",
+    appId: "1:675416219162:web:78958919f2fa479b19fba3"
+  }
 };
+
