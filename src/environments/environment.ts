b38505bab5058d9d0e9970f293644379ebae22e3
Clemo97 <lumumbaclement@gmail.com>
2024-02-17 11:22:41 +0300

    add google analytics

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,6 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
 export const environment = {
   production: true,
   firebase: {
@@ -6,7 +9,11 @@ export const environment = {
     projectId: "sil-hosting",
     storageBucket: "sil-hosting.appspot.com",
     messagingSenderId: "675416219162",
-    appId: "1:675416219162:web:78958919f2fa479b19fba3"
+    appId: "1:675416219162:web:78958919f2fa479b19fba3",
+    measurementId: "G-XXKXJZWNCS"
   }
 };
 
+// Initialize Firebase
+const app = initializeApp(environment.firebase);
+const analytics = getAnalytics(app);
